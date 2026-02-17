a1803bfda13ff76a40af4baba440d38d848201fe
Rajan Maurya <rajan12maurya2008@gmail.com>
2026-02-17 23:12:07 +0530

    Removed database.py from repo

--- database.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-def connect_db():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        passsword="rajanmysql",
-        database="exam_portal"
-    )
-
